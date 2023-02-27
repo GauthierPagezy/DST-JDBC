d26661c993b92446355c26a6d8d5a63ee8ea4ab8
GauthierPagezy <80842027+GauthierPagezy@users.noreply.github.com>
2023-02-27 17:22:17 +0100

    Add files via upload

--- src/Model/Repository2.java
+++ src/Model/Repository2.java
@@ -6,15 +6,14 @@ import java.sql.SQLException;
 
 
 public class Repository2 {
-    java.sql.PreparedStatement pstmt = null;
+    java.sql.Statement stmt = null;
     ResultSet resultat = null;
 
     public ResultSet selectNoteByIdCand(Connection conn, String idCand) {
         String sql = "SELECT note FROM resultat WHERE idCand = ?";
         try {
-            this.pstmt =  conn.prepareStatement(sql);
-            pstmt.setString(1,idCand);
-            this.resultat = pstmt.executeQuery();
+            this.stmt =  conn.createStatement();
+            this.resultat = stmt.executeQuery(sql);
         } catch (SQLException e) {
             System.out.println("Erreur lors du chargement "+e.getMessage());
         }
